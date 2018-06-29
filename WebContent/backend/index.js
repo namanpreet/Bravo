4a0c4456184c5c680e1ccedd0204b181cffb829e
Namanpreet Singh <n3singh@bravurasolutions.com>
2018-06-29 15:14:37 +0530

    Backend code changes

---
+++ WebContent/backend/index.js
@@ -0,0 +1,10 @@
+var express=require("express");
+var bodyParser=require('body-parser');
+var app = express();
+var authenticateController=require('./controllers/authenticate-controller');
+var registerController=require('./controllers/register-controller');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());// parsing 
+app.post('/api/register',registerController.register);
+app.post('/api/authenticate',authenticateController.authenticate);
+app.listen(8012);
